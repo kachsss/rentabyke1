8dad65d01fd6fd49b4f3592e9ac68bd16abd7f58
kachsss <kachxxx@gmail.com>
2015-10-15 18:06:58 +0400

    old_css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Renta Bike in Tallin"
+    base_title = "Прокат велосипедов в Таллине"
     if page_title.empty?
       base_title
     else
